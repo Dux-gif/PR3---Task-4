6d568ed01a8f91f02b55ebd6b8ef3e78ecb78db7
user <user@DESKTOP-SECB52V>
2024-03-31 17:03:10 +0500

    Добавьте файлы проекта.

---
+++ PR3 - Task 4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR3___Task_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите длину последовательности: ");
+            int n = int.Parse(Console.ReadLine());
+            int num = 0;
+            int min = int.MaxValue;
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Введите число: ");
+                num = int.Parse(Console.ReadLine());
+                if (num < min)
+                {
+                    min = num;
+                }
+            }
+            Console.WriteLine("Наименьшее число: " + min);
+            Console.ReadKey();
+        }
+    }
+}
